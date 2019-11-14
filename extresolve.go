f3845d1bbaad27ae8a8de36aeabf5527794aaa52
Dmitriy Vlassov <vlassov@kolesa.kz>
2019-11-14 11:20:05 +0600

    some improvements

--- extresolve.go
+++ extresolve.go
@@ -17,20 +17,85 @@ import (
 var app = cli.NewApp()
 var tab tabular.Table
 
-type DNS []struct {
-	IP          string      `json:"ip"`
-	Name        string      `json:"name"`
-	CountryID   string      `json:"country_id"`
-	City        string      `json:"city"`
-	Version     string      `json:"version"`
-	Error       interface{} `json:"error"`
-	Dnssec      bool        `json:"dnssec"`
-	Reliability float64     `json:"reliability"`
-	CheckedAt   time.Time   `json:"checked_at"`
-	CreatedAt   time.Time   `json:"created_at"`
+type DnsRecord struct {
+	IP          string  `json:"ip"`
+	Name        string  `json:"name"`
+	City        string  `json:"city"`
+	Dnssec      bool    `json:"dnssec"`
+	Reliability float64 `json:"reliability"`
 }
 
-func init() {
+func chechHost(c *cli.Context) (err error) {
+	count := c.Int("count")
+
+	protocol := c.String("protocol")
+
+	host := c.String("host")
+
+	geo := c.String("region")
+
+	timeout := c.Int("timeout")
+
+	timeoutGetDns := c.Int("timeout-get-dns-list")
+
+	detail := c.Bool("detail")
+
+	var httpClient = &http.Client{
+		Timeout: time.Duration(timeoutGetDns) * time.Second,
+	}
+	resp, err := httpClient.Get("https://public-dns.info/nameserver/" + geo + ".json")
+	if err != nil {
+		return error(err)
+	}
+	f := func(bool) string {
+		if detail {
+			return tab.Print("DNS", "Name", "City", "Dnssec", "Reliability", "Result")
+		} else {
+			return tab.Print("DNS", "Name", "City", "Result")
+		}
+	}
+	format := f(detail)
+
+	if body, err := ioutil.ReadAll(resp.Body); err == nil {
+		var dnsHosts []DnsRecord
+		if err = json.Unmarshal(body, &dnsHosts); err == nil {
+			for id, record := range dnsHosts {
+				var resolver *net.Resolver
+				resolver = &net.Resolver{
+					PreferGo: true,
+					Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+						d := net.Dialer{
+							Timeout: time.Duration(timeout) * time.Second,
+						}
+						return d.DialContext(ctx, protocol, net.JoinHostPort(record.IP, "53"))
+					},
+				}
+				ips, _ := resolver.LookupIPAddr(context.Background(), host)
+				var resolveHost = ""
+				for _, ip := range ips {
+					resolveHost = resolveHost + " " + ip.String()
+				}
+				if detail {
+					fmt.Printf(format, record.IP, record.Name, record.City, record.Dnssec, record.Reliability, resolveHost)
+				} else {
+					fmt.Printf(format, record.IP, record.Name, record.City, resolveHost)
+				}
+
+				if (id + 1) == count {
+					break
+				}
+			}
+		} else {
+			return error(err)
+		}
+	} else {
+		return error(err)
+	}
+	defer resp.Body.Close()
+	return nil
+}
+
+func main() {
 	// Cli settings
 	app.Name = "Dns check"
 	app.Version = "1.0"
@@ -91,80 +156,11 @@ func init() {
 	tab.Col("Dnssec", "DNS security", 12)
 	tab.Col("Reliability", "DNS reliability", 15)
 	tab.Col("Result", "DNS response", 0)
-}
-
-func chechHost(c *cli.Context) {
-	count := c.Int("count")
-
-	protocol := c.String("protocol")
-
-	host := c.String("host")
-
-	geo := c.String("region")
-
-	timeout := c.Int("timeout")
-
-	timeoutGetDns := c.Int("timeout-get-dns-list")
-
-	detail := c.Bool("detail")
-
-	var httpClient = &http.Client{
-		Timeout: time.Duration(timeoutGetDns) * time.Second,
-	}
-	resp, err := httpClient.Get("https://public-dns.info/nameserver/" + geo + ".json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	f := func(bool) string {
-		if detail {
-			return tab.Print("DNS", "Name", "City", "Dnssec", "Reliability", "Result")
-		} else {
-			return tab.Print("DNS", "Name", "City", "Result")
-		}
-	}
-	format := f(detail)
-
-	if body, err := ioutil.ReadAll(resp.Body); err == nil {
-		var dnsHosts = DNS{}
-		if err = json.Unmarshal(body, &dnsHosts); err == nil {
-			for id, row := range dnsHosts {
-				var resolver *net.Resolver
-				resolver = &net.Resolver{
-					PreferGo: true,
-					Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
-						d := net.Dialer{
-							Timeout: time.Duration(timeout) * time.Second,
-						}
-						return d.DialContext(ctx, protocol, net.JoinHostPort(row.IP, "53"))
-					},
-				}
-				ips, _ := resolver.LookupIPAddr(context.Background(), host)
-				var resolveHost = ""
-				for _, ip := range ips {
-					resolveHost = resolveHost + " " + ip.String()
-				}
-				if detail {
-					fmt.Printf(format, row.IP, row.Name, row.City, row.Dnssec, row.Reliability, resolveHost)
-				} else {
-					fmt.Printf(format, row.IP, row.Name, row.City, resolveHost)
-				}
-
-				if (id + 1) == count {
-					break
-				}
-			}
-		} else {
+	//
+	app.Action = func(c *cli.Context) {
+		if err := chechHost(c); err != nil {
 			log.Fatal(err)
 		}
-	} else {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-}
-
-func main() {
-	app.Action = func(c *cli.Context) {
-		chechHost(c)
 	}
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
